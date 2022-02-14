5153b850e1cbac360d635d976dbb2d5047ea2ec5
Vika Ruban <V_I_C_K_A_2_8@mail.ru>
2022-02-14 18:07:36 +0300

    проектная работа4_п1

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+let buttonSave = document.querySelector('.popup__button-save');
+buttonSave.addEventListener('click', function () {
+  let popupName = document.querySelector('.popup__name');
+  let popupDescription = document.querySelector('.popup__description');
+  let profileName = document.querySelector('.profile__name');
+  let profileDescription = document.querySelector('.profile__description');
+  if (popupName.value !== ''){
+  profileName.textContent = popupName.value;
+    }
+  if (popupDescription.value !== '') {
+  profileDescription.textContent = popupDescription.value;
+    }
+    let popup = document.querySelector('.popup');
+    popup.classList.remove("popup_opened");
+  } 
+)
+let buttonExit = document.querySelector('.popup__button-exit')
+buttonExit.addEventListener('click', function () {
+  let popup = document.querySelector('.popup');
+  popup.classList.remove("popup_opened");
+  }
+)
+let buttonProfileEdit = document.querySelector('.profile__button-edit');
+buttonProfileEdit.addEventListener('click', function () { 
+  let popup = document.querySelector('.popup');
+  popup.classList.add("popup_opened");
+  }
+)
+
+/*
+let profileButton = document.querySelector('.profile__button');
+profileButton.addEventListener('click', function () {
+  let popup = document.querySelector('.popup');
+  popup.classList.add("popup_opened");
+  }
+)*/
