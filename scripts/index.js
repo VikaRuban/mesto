613aa594434c77e4b1a6895aa9c0fa561375827d
Vika Ruban <V_I_C_K_A_2_8@mail.ru>
2022-02-17 19:12:44 +0300

    проектная работа4_п2

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,33 @@
 const buttonSave = document.querySelector('.popup__button-save');
-buttonSave.addEventListener('click', function () {
-  let popupName = document.querySelector('.popup__name');
-  let popupDescription = document.querySelector('.popup__description');
-  let profileName = document.querySelector('.profile__name');
-  let profileDescription = document.querySelector('.profile__description');
+const popupName = document.querySelector('.popup__name');
+const popupDescription = document.querySelector('.popup__description');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
+const buttonExit = document.querySelector('.popup__button-exit')
+const buttonProfileEdit = document.querySelector('.profile__button-edit');
+const popup = document.querySelector('.popup');
+
+function inputCompare (e) {
   if (popupName.value !== '') {
     profileName.textContent = popupName.value;
     }
   if (popupDescription.value !== '') {
     profileDescription.textContent = popupDescription.value;
     }
-  /*закрыть попап через save*/
-  let popup = document.querySelector('.popup');
+  e.preventDefault()
+}
+function currentInfo() {
+  popupName.value = profileName.textContent;
+  popupDescription.value = profileDescription.textContent;
+}
+function popupClose() {
   popup.classList.remove("popup_opened");
-  }
-)
-/*закрыть попап через х*/
-let buttonExit = document.querySelector('.popup__button-exit')
-buttonExit.addEventListener('click', function () {
-  let popup = document.querySelector('.popup');
-  popup.classList.remove("popup_opened");
-  }
-)
-/*открыть попап через edit*/
-let buttonProfileEdit = document.querySelector('.profile__button-edit');
-buttonProfileEdit.addEventListener('click', function () { 
-  let popup = document.querySelector('.popup');
+}
+function popupOpen() {
   popup.classList.add("popup_opened");
-  }
-)
+  currentInfo(); /*добавление инфы в инпуты*/
+}
+buttonSave.addEventListener('click', inputCompare); /*сохранение инфы*/
+buttonSave.addEventListener('click', popupClose);  /*закрыть попап через save*/
+buttonExit.addEventListener('click', popupClose); /*закрыть попап через х*/
+buttonProfileEdit.addEventListener('click', popupOpen) /*открыть попап через edit*/
